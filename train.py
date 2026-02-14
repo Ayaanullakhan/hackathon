978f80d04524ea639718d69c6cc853580fff910c
Ayaanullakhan <ayaanullakhan11@gmail.com>
2026-02-15 02:04:42 +1100

    training changes

--- train.py
+++ train.py
@@ -1,19 +1,19 @@
 import os
-from ultralytics import YOLO
+from ultralytics import YOLO  
 
-os.environ["ULTRALYTICS_DATASETS_DIR"] = r"C:\Users\ayans\VSCodeProjects\Construction-RIsk-Analyzer\hackathon\datasets"
+os.environ["ULTRALYTICS_DATASETS_DIR"] = r"C:\Users\ayans\VSCodeProjects\hackathon\datasets\construction-ppe"
 
 model = YOLO("yolo11n.pt")
 
 model.train(
     data="construction-ppe-local.yaml",
-    epochs=8,
+    epochs=15,
     imgsz=512,
     batch=4,
     workers=0,
     device="cpu",
     patience=3,
-    project=r"C:\Users\ayans\VSCodeProjects\Construction-RIsk-Analyzer\hackathon\runs",
+    project=r"C:\Users\ayans\VSCodeProjects\hackathon\runs",
     name="ppe_fast",
     pretrained=True,
     freeze=10
